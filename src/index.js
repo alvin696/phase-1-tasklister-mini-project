96ce0fa2c5179df533aaff998254160984ca1fdd
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-03-27 14:22:26 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector('form');
+  const taskInput = document.querySelector('#task');
+  const taskList = document.querySelector('#task-list');
+
+  function addTask(event) {
+    // prevent default form submission
+    event.preventDefault();
+
+    // get the task from the input field
+    const taskText = taskInput.value;
+
+    // create a new list item with the task text
+    const taskItem = document.createElement('li');
+    taskItem.innerText = taskText;
+
+    // add the new list item to the task list
+    taskList.appendChild(taskItem);
+
+    // clear the input field
+    taskInput.value = '';
+  }
+
+  // listen for form submission
+  taskForm.addEventListener('submit', addTask);
 });
